3689d737b401dee1c76ccf1ef2e151d466dce284
LeonidFedoro <115628712+LeonidFedoro@users.noreply.github.com>
2024-06-06 04:40:28 +0700

    Delete Class directory

--- Class/Match.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from Class.User import User
-
-class Match:
-    def __init__(self, match_id: int, user1: User, user2: User, match_date: datetime):
-        self.match_id = match_id
-        self.user1 = user1
-        self.user2 = user2
-        self.match_date = match_date
--- Class/User.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, user_id: int, name: str, age: int, gender: str, email: str):
-        self.user_id = user_id
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.email = email
